d0a25567f886f8155f33a7db3cf3c2ddc495a2b1
faramarziha <69faramarz@gmail.com>
2025-08-20 00:27:15 +0330

    Remove Django admin and unused user profile events

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Device
-
-@admin.register(Device)
-class DeviceAdmin(admin.ModelAdmin):
-    list_display = ("name", "is_active", "last_seen")
